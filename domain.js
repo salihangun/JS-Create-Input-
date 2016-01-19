0f87707019dae142358ee33331c2afb8291ef5ff
Salih Angün <salih_angun@hotmail.com>
2016-01-19 08:28:12 +0200

    js input oluşturma

---
+++ domain.js
@@ -0,0 +1,51 @@
+﻿var Domain = function () {
+    var base = this;
+
+    this.Title = 'Parent Entity';
+
+    this.Entity = function () {
+        this.Base = base;       
+
+        this.Key = 'entity';
+        this.Name = 'Entity';        
+        this.PluralName = 'Entities';        
+        
+        this.PrimaryButton = 'Save';
+        this.SecondaryButton = 'Save and New';
+
+        // only 1 or 2 supported
+        this.FormColumnCount = 2;
+        this.Inputs = [];
+        this.NotDisplayedInputs = []; 
+
+        var displayNone = { Label: 'Not Displayed Text', Name: 'TextHidden', Type: 'text', Disabled: true, Display: true };
+        this.NotDisplayedInputs.push(displayNone);
+        for (var i = 0; i < 100; i++) {
+            var text = { Label: 'Text', Name: 'Text'+i+'', Type: 'text' };
+            var textDisabled = { Label: 'Text Disabled', Name: 'TextDisabled' + i + '', Type: 'text', Disabled: true };
+            var date = { Label: 'Date', Name: 'Date' + i + '', Type: 'date' };
+            var checkbox = { Label: 'Checkbox', Name: 'Checkbox' + i + '', Type: 'checkbox' };
+            var file = { Label: 'File', Name: 'File' + i + '', Type: 'file' };
+            var fileMultiple = { Label: 'File Multiple', Name: 'FileMultiple' + i + '', Type: 'file', Multiple: true };
+            var select = { Label: 'Select', Name: 'Select' + i + '', Type: 'select', Items: ['A', 'B', 'C'] };
+            var selectDisabled = { Label: 'Select', Name: 'Select' + i + '', Type: 'select', Items: ['A', 'B', 'C'], Disabled: true };
+            var selectMultiple = { Label: 'Select', Name: 'Select' + i + '', Type: 'select', Items: ['A', 'B', 'C'], Multiple: true };
+
+            this.Inputs.push(displayNone);
+            this.Inputs.push(text);
+            this.Inputs.push(textDisabled);
+            this.Inputs.push(date);
+            this.Inputs.push(checkbox);
+            this.Inputs.push(file);
+            this.Inputs.push(fileMultiple);
+            this.Inputs.push(select);
+            this.Inputs.push(selectDisabled);
+            this.Inputs.push(selectMultiple);
+        }
+       
+
+      
+    };
+}
+
+var proje = new Domain();
---
+++ form.js
@@ -0,0 +1,78 @@
+﻿function setTodayForDateFields() {
+    var now = new Date();
+    var month = (now.getMonth() + 1);
+    var day = now.getDate();
+    if (month < 10)
+        month = "0" + month;
+    if (day < 10)
+        day = "0" + day;
+    var today = now.getFullYear() + '-' + month + '-' + day;
+    $('input[type=date]').val(today);
+}
+
+Array.prototype.any = function (property, value) {
+    if (property == undefined) {
+        console.log('please define property to use any with an array (array.any("prop","value"))');
+        return false;
+    }
+
+    if (this.length < 1) { return false; }
+
+    for (var i = 0; i < this.length; i++) {
+        if (this[i][property] == value) { return true; }
+    }
+
+    return false;
+}
+
+var load_view = {
+    New: function (entity) {
+        var form = $('<form id='+ entity.Name +' />');
+       
+        for (var i = 0; i < entity.Inputs.length; i++) {
+            var item = entity.Inputs[i];
+            
+            if (entity.NotDisplayedInputs.any('Name', item.Name)) { continue; };
+
+            var input = $('<input type="' + item.Type + '" id="' + item.Name + '" name="' + item.Name + '"  />');     
+            var div = $('<div />');
+
+            if (item.Type == 'select') {
+                input = $('<select id="' + item.Name + '" name="' + item.Name + '"/>');
+
+                for (var j = 0; j < item.Items.length; j++) {
+                    var opt = item.Items[j];
+                    if (opt.value) {
+                        input.append('<option value="' + opt.value + '">' + opt.name + '</option>');
+                    }
+                    else {
+                        input.append('<option>' + opt + '</option>');
+                    }
+                }
+            }    
+
+            input.attr('disabled', item.Disabled === true);
+            input.attr('multiple', item.Multiple === true);
+            input.val(item.Value ? item.Value : '');
+            
+            div.append('<label>' + item.Label + '</label>');
+            div.append(input);
+            form.append(div);            
+        }
+
+        var formFooter = $('<div class="formFooter">');
+
+        //form buttons
+        if (entity.PrimaryButton) { formFooter.append('<button id="btnPrimary" type="button" class="colorPrimary">' + entity.PrimaryButton + '</button>'); };
+        if (entity.SecondaryButton) { formFooter.append('<button id="btnSecondary" type="button" class="colorSecondary">' + entity.SecondaryButton + '</button>'); };
+        if (entity.ImportantButton) { formFooter.append('<button id="btnImportant" type="button" class="colorImportant">' + entity.ImportantButton + '</button>'); };
+        
+        form.append(formFooter);        
+        $('#main').append(form);
+
+        if (entity.FormColumnCount == 2) { $('form > div').addClass('formColumn'); };
+
+        //default values
+        setTodayForDateFields(); 
+    }
+};
